f9e9a01a6b806a1f5baf6ea9c8b04e4c04adc240
nimitsajal99 <53811291+nimitsajal99@users.noreply.github.com>
2020-03-03 11:41:35 +0530

    Add files via upload

---
+++ insertion.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+	int i, key, j;
+	for(i=1; i<n; i++)
+	{
+		key = arr[i];
+		j = i -1;
+		while(j>=0 && arr[j]>key)
+		{
+			arr[j+1] = arr[j];
+			j=j-1;
+		}
+		arr[j+1] = key;
+	}
+}
+
+void printArray(int arr[], int n)
+{
+	int i;
+	for(i=0; i<n; i++)
+		cout << arr[i] << " ";
+	cout <<endl;
+}
+
+int main()
+{
+	
+	int n, arr[100];
+	cout << "Enter array Length" <<endl; 
+	cin >> n;
+	cout<< "Enter array of size " << n <<endl;
+	for(int i = 0; i<n; i++)
+		cin >> arr[i];
+	insertionSort(arr, n);
+	printArray(arr,n);
+
+	return 0;
+}
