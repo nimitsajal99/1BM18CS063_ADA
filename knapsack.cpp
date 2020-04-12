0836a3ff8ad74ec2dbced30d8dd335576d61cbfb
nimitsajal99 <53811291+nimitsajal99@users.noreply.github.com>
2020-04-12 16:51:46 +0530

    Add files via upload

---
+++ knapsack.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+int max(int a, int b) { return (a > b)? a : b; }  
+  
+int knapSack(int W, int wt[], int val[], int n)  
+{  
+      
+if (n == 0 || W == 0)  
+    return 0;  
+  
+if (wt[n-1] > W)  
+    return knapSack(W, wt, val, n-1);  
+  
+else return max( val[n-1] + knapSack(W-wt[n-1], wt, val, n-1),  
+                    knapSack(W, wt, val, n-1) );  
+}  
+  
+int main()  
+{  
+    int val[] = {70, 120, 220};  
+    int wt[] = {15, 30, 50};  
+    int W = 90;  
+    int n = sizeof(val)/sizeof(val[0]);  
+    cout<<knapSack(W, wt, val, n);  
+    return 0;  
+}
